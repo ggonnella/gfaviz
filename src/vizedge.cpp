83dc0f05bd9254ec6a41bf256288c7a60613dbb5
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2018-09-10 16:52:57 +0200

    vizedge.cpp, cosmetics

--- src/vizedge.cpp
+++ src/vizedge.cpp
@@ -1,13 +1,10 @@
 #include "vizedge.h"
-
 #include "vizgraph.h"
 #include "vecops.h"
-
 #include <QPainterPathStroker>
 
-
-
-VizEdge::VizEdge(GfaEdge* _gfa_edge, VizGraph* _vg) : VizElement(VIZ_EDGE, _vg, _gfa_edge) {
+VizEdge::VizEdge(GfaEdge* _gfa_edge, VizGraph* _vg) :
+                                          VizElement(VIZ_EDGE, _vg, _gfa_edge) {
   gfa_edge = _gfa_edge;
   isDovetail = gfa_edge->isDovetail();
   highlights[0] = NULL;
@@ -15,20 +12,25 @@ VizEdge::VizEdge(GfaEdge* _gfa_edge, VizGraph* _vg) : VizElement(VIZ_EDGE, _vg, 
   viz_nodes[0] = vg->getNode(gfa_edge->getSegment(0));
   viz_nodes[1] = vg->getNode(gfa_edge->getSegment(1));
   if (isDovetail) {
-    connected_subnodes[0] = (gfa_edge->isInedge(0) ? viz_nodes[0]->getStart() : viz_nodes[0]->getEnd());
-    connected_subnodes[1] = (gfa_edge->isInedge(1) ? viz_nodes[1]->getStart() : viz_nodes[1]->getEnd());
+    connected_subnodes[0] = (gfa_edge->isInedge(0) ?
+        viz_nodes[0]->getStart() : viz_nodes[0]->getEnd());
+    connected_subnodes[1] = (gfa_edge->isInedge(1) ?
+        viz_nodes[1]->getStart() : viz_nodes[1]->getEnd());
   } else {
-    connected_subnodes[0] = viz_nodes[0]->getNodeAtBase((gfa_edge->getBegin(0)+gfa_edge->getEnd(0))/2);
-    connected_subnodes[1] = viz_nodes[1]->getNodeAtBase((gfa_edge->getBegin(1)+gfa_edge->getEnd(1))/2);
+    connected_subnodes[0] = viz_nodes[0]->getNodeAtBase(
+        (gfa_edge->getBegin(0)+gfa_edge->getEnd(0))/2);
+    connected_subnodes[1] = viz_nodes[1]->getNodeAtBase(
+        (gfa_edge->getBegin(1)+gfa_edge->getEnd(1))/2);
   }
   if (gfa_edge->positionsSet()) {
-    highlights[0] = viz_nodes[0]->registerHighlight(gfa_edge->getBegin(0), gfa_edge->getEnd(0));
-    highlights[1] = viz_nodes[1]->registerHighlight(gfa_edge->getBegin(1), gfa_edge->getEnd(1));
+    highlights[0] = viz_nodes[0]->registerHighlight(gfa_edge->getBegin(0),
+        gfa_edge->getEnd(0));
+    highlights[1] = viz_nodes[1]->registerHighlight(gfa_edge->getBegin(1),
+        gfa_edge->getEnd(1));
     highlights[0]->setVisible(false);
     highlights[1]->setVisible(false);
   }
-  
-  
+
   ogdf_edge = vg->G.searchEdge(connected_subnodes[0], connected_subnodes[1]);
   if (ogdf_edge == 0)
     ogdf_edge = vg->G.newEdge(connected_subnodes[0], connected_subnodes[1]);
@@ -38,7 +40,7 @@ VizEdge::VizEdge(GfaEdge* _gfa_edge, VizGraph* _vg) : VizElement(VIZ_EDGE, _vg, 
   } else {
     vg->GA.doubleWeight(ogdf_edge) = 80;
   }
-  
+
   setAcceptHoverEvents(true);
   setFlags(ItemIsSelectable);
   setAcceptedMouseButtons(Qt::AllButtons);
@@ -47,21 +49,25 @@ VizEdge::VizEdge(GfaEdge* _gfa_edge, VizGraph* _vg) : VizElement(VIZ_EDGE, _vg, 
 }
 
 VizEdge::~VizEdge() {
-  
+  /* empty */
 }
 
 QPainterPath VizEdge::getPath(VizGroup* group) {
   QPainterPath path;
   if (!isDovetail) {
-    QPointF p1 = viz_nodes[0]->getCoordForBase((gfa_edge->getBegin(0)+gfa_edge->getEnd(0))/2);
-    QPointF p2 = viz_nodes[1]->getCoordForBase((gfa_edge->getBegin(1)+gfa_edge->getEnd(1))/2);
+    QPointF p1 = viz_nodes[0]->getCoordForBase(
+        (gfa_edge->getBegin(0)+gfa_edge->getEnd(0))/2);
+    QPointF p2 = viz_nodes[1]->getCoordForBase(
+        (gfa_edge->getBegin(1)+gfa_edge->getEnd(1))/2);
     path.moveTo(p1);
     path.lineTo(p2);
   } else {
     QPointF p1 = vg->getNodePos(connected_subnodes[0]);
     QPointF p2 = vg->getNodePos(connected_subnodes[1]);
-    QPointF d1 = (gfa_edge->isInedge(0) ? viz_nodes[0]->getStartDir() : viz_nodes[0]->getEndDir());
-    QPointF d2 = (gfa_edge->isInedge(1) ? viz_nodes[1]->getStartDir() : viz_nodes[1]->getEndDir());
+    QPointF d1 = (gfa_edge->isInedge(0) ?
+        viz_nodes[0]->getStartDir() : viz_nodes[0]->getEndDir());
+    QPointF d2 = (gfa_edge->isInedge(1) ?
+        viz_nodes[1]->getStartDir() : viz_nodes[1]->getEndDir());
     path.moveTo(p1);
     path.cubicTo(p1+5*d1, p2+5*d2, p2);
   }
@@ -82,10 +88,10 @@ QPainterPath VizEdge::getPath(VizGroup* group) {
   return path;
 }
 
-void VizEdge::draw() {  
+void VizEdge::draw() {
   //if (scene())
     //vg->scene->removeItem(this);
-  
+
   setPath(getPath());
   if (!isDovetail) {
     QPen pen(getOption(VIZ_INTERNALCOLOR).value<QColor>());
@@ -96,7 +102,7 @@ void VizEdge::draw() {
     pen.setWidthF(getOption(VIZ_DOVETAILWIDTH).toDouble());
     setPen(pen);
   }
-  
+
   bool visible = getOption(VIZ_EDGEHIGHLIGHTSHOW).toBool();
   QColor color = getOption(VIZ_EDGEHIGHLIGHTCOLOR).value<QColor>();
   if (highlights[0]) {
@@ -107,7 +113,7 @@ void VizEdge::draw() {
     highlights[1]->setVisibility(visible);
     highlights[1]->setColor(color);
   }
-  
+
   if (getOption(VIZ_EDGELABELSHOW).toBool()) {
     drawLabel(getOption(VIZ_EDGELABELFONT).toString(),
               getOption(VIZ_EDGELABELFONTSIZE).toDouble(),
@@ -146,7 +152,7 @@ void VizEdge::hoverEnterEvent(QGraphicsSceneHoverEvent *e) {
   }
   setPen(pen);
   update();
-  
+
   if (highlights[0]) {
     highlights[0]->setVisibility(true);
     highlights[0]->draw();
@@ -155,9 +161,10 @@ void VizEdge::hoverEnterEvent(QGraphicsSceneHoverEvent *e) {
     highlights[1]->setVisibility(true);
     highlights[1]->draw();
   }
-  
+
   VizElement::hoverEnterEvent(e);
 }
+
 void VizEdge::hoverLeaveEvent(QGraphicsSceneHoverEvent *e) {
   QPen pen;
   if (!isDovetail) {
@@ -169,13 +176,13 @@ void VizEdge::hoverLeaveEvent(QGraphicsSceneHoverEvent *e) {
   }
   setPen(pen);
   update();
-  
+
   if (!getOption(VIZ_EDGEHIGHLIGHTSHOW).toBool()) {
     if (highlights[0])
       highlights[0]->setVisibility(false);
     if (highlights[1])
       highlights[1]->setVisibility(false);
   }
-  
+
   VizElement::hoverLeaveEvent(e);
 }
